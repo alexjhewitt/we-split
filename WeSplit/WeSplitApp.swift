254775bd38aeca9a6e6318f1178e1247c57a8ef0
Alex Hewitt <alexhewitt.j@gmail.com>
2025-06-12 21:42:03 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alex Hewitt on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
